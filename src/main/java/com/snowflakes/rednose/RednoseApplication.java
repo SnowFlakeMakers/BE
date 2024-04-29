3302df940f19ebedbab039956bb019ff94a921c1
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-04-29 19:37:03 +0900

    CHORE: 초기 설정

---
+++ src/main/java/com/snowflakes/rednose/RednoseApplication.java
@@ -0,0 +1,13 @@
+package com.snowflakes.rednose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RednoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RednoseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snowflakes/rednose/RednoseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowflakes.rednose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RednoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
