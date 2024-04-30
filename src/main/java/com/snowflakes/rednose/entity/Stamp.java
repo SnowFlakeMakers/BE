25d8e8e90135ef871b1eb39af462b09bb69fec8f
rlfrkdms1 <rlfrkdms1@naver.com>
2024-04-30 16:40:15 +0900

    feat: Stamp 엔티티 추가
    Related to: #3

---
+++ src/main/java/com/snowflakes/rednose/entity/Stamp.java
@@ -0,0 +1,39 @@
+package com.snowflakes.rednose.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import org.springframework.data.annotation.CreatedDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class Stamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    public Stamp() {
+    }
+
+    @Builder
+    public Stamp(Long id, String name, String imageUrl, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.createdAt = createdAt;
+    }
+}
