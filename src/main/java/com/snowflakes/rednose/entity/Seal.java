bf1109eca3ea9ad8d859f7fff0b07386ecd1eaaf
rlfrkdms1 <rlfrkdms1@naver.com>
2024-04-30 16:38:46 +0900

    feat: Seal 엔티티 추가
    Related to: #3

---
+++ src/main/java/com/snowflakes/rednose/entity/Seal.java
@@ -0,0 +1,48 @@
+package com.snowflakes.rednose.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import org.springframework.data.annotation.CreatedDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class Seal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    public Seal() {
+    }
+
+    @Builder
+    public Seal(Long id, Member member, String name, String imageUrl, LocalDateTime createdAt) {
+        this.id = id;
+        this.member = member;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.createdAt = createdAt;
+    }
+
+}
