b7791e5d28ac917c391d93f4dc4a5f18cd5420bc
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-02 17:53:06 +0900

    feat: 좋아요 기능 추가
    좋아요를 눌렀을 때 이를 저장하는 기능을 추가합니다.
    
    Related to: #3

---
+++ src/main/java/com/snowflakes/rednose/service/StampLikeService.java
@@ -0,0 +1,31 @@
+package com.snowflakes.rednose.service;
+
+import com.snowflakes.rednose.entity.Member;
+import com.snowflakes.rednose.entity.Stamp;
+import com.snowflakes.rednose.entity.StampLike;
+import com.snowflakes.rednose.exception.NotFoundException;
+import com.snowflakes.rednose.repository.MemberRepository;
+import com.snowflakes.rednose.repository.StampLikeRepository;
+import com.snowflakes.rednose.repository.StampRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.snowflakes.rednose.exception.ErrorCode.MEMBER_NOT_FOUND;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class StampLikeService {
+
+    private final StampLikeRepository stampLikeRepository;
+    private final StampRepository stampRepository;
+    private final MemberRepository memberRepository;
+
+    public void like(Long stampId, Long memberId) {
+        Stamp stamp = stampRepository.findById(stampId).orElseThrow(() -> new NotFoundException(MEMBER_NOT_FOUND));
+        Member member= memberRepository.findById(memberId).orElseThrow(() -> new NotFoundException(MEMBER_NOT_FOUND));
+        StampLike like = StampLike.builder().stamp(stamp).member(member).build();
+        stampLikeRepository.save(like);
+    }
+}
