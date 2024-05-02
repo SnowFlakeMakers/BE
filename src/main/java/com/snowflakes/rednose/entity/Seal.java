d3359f2ea3cb1d1c1d32c0b22086227e931bc9dc
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-02 18:32:59 +0900

    feat: @createdDate 세팅 추가
    Related to: #3

--- src/main/java/com/snowflakes/rednose/entity/Seal.java
+++ src/main/java/com/snowflakes/rednose/entity/Seal.java
@@ -2,6 +2,7 @@ package com.snowflakes.rednose.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,9 +11,11 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Seal {
 
     @Id
--- src/main/java/com/snowflakes/rednose/entity/Stamp.java
+++ src/main/java/com/snowflakes/rednose/entity/Stamp.java
@@ -2,14 +2,18 @@ package com.snowflakes.rednose.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Stamp {
 
     @Id
