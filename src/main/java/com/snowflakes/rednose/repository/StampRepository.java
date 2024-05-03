29c826c5142e02852c44513ca9db0abeceb137fc
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-04 00:01:01 +0900

    feat: memberId로 좋아요 누른 우표 조회 기능 추가
    Related to: #3

--- src/main/java/com/snowflakes/rednose/repository/StampRepository.java
+++ src/main/java/com/snowflakes/rednose/repository/StampRepository.java
@@ -1,11 +1,17 @@
 package com.snowflakes.rednose.repository;
 
 import com.snowflakes.rednose.entity.Stamp;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface StampRepository extends JpaRepository<Stamp, Long> {
 
     Optional<Stamp> findById(Long stampId);
+
+    @Query("select s from Stamp s join StampLike sl where sl.member.id = :memberId")
+    Slice<Stamp> findLikesByMemberId(@Param("memberId") Long memberId);
+
 }
