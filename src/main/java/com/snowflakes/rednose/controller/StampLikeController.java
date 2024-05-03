da8f5778f0d3a6bde47ef2225f2814663d63e36c
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-04 00:01:21 +0900

    feat: 좋아요 누른 우표 조회 컨트롤러 추가
    Related to: #3

--- src/main/java/com/snowflakes/rednose/controller/StampLikeController.java
+++ src/main/java/com/snowflakes/rednose/controller/StampLikeController.java
@@ -1,8 +1,10 @@
 package com.snowflakes.rednose.controller;
 
+import com.snowflakes.rednose.dto.like.stamp.ShowStampLikeResponse;
 import com.snowflakes.rednose.service.StampLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,11 @@ public class StampLikeController {
     public ResponseEntity<Void> like(Long memberId, @PathVariable Long stampId) {
         stampLikeService.like(stampId, memberId);
         return ResponseEntity.created(URI.create("/api/v1/stamps")).build();
+    }
+
+    @GetMapping("/likes")
+    public ShowStampLikeResponse show(Long memberId) {
+        return stampLikeService.getLikes(memberId);
     }
 
 }
