60068b05a23742524a683518bda06580a657de9f
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-03 23:54:29 +0900

    feat: 좋아요 누른 우표들 조회 기능 추가
    Related to: #3

--- src/main/java/com/snowflakes/rednose/service/StampLikeService.java
+++ src/main/java/com/snowflakes/rednose/service/StampLikeService.java
@@ -15,17 +15,23 @@ import static com.snowflakes.rednose.exception.ErrorCode.MEMBER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class StampLikeService {
 
     private final StampLikeRepository stampLikeRepository;
     private final StampRepository stampRepository;
     private final MemberRepository memberRepository;
 
+    @Transactional
     public void like(Long stampId, Long memberId) {
         Stamp stamp = stampRepository.findById(stampId).orElseThrow(() -> new NotFoundException(MEMBER_NOT_FOUND));
         Member member= memberRepository.findById(memberId).orElseThrow(() -> new NotFoundException(MEMBER_NOT_FOUND));
         StampLike like = StampLike.builder().stamp(stamp).member(member).build();
         stampLikeRepository.save(like);
+    }
+
+    public ShowStampLikeResponse getLikes(Long memberId) {
+        Slice<Stamp> stamps = stampRepository.findLikesByMemberId(memberId);
+        return ShowStampLikeResponse.from(stamps);
     }
 }
