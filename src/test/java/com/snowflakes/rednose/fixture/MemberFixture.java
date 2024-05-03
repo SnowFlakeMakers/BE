bb54b5319d4392954f0b44cd3de6537c8cf5a2dc
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-03 15:18:27 +0900

    feat: fixture 추가
    test를 위한 fixture 추가입니다.
    - member
    - stamp
    enum 형식의 fixture가 아닌 class로 builder 패턴의 응용 방법입니다.
    
    Related to: #3

---
+++ src/test/java/com/snowflakes/rednose/fixture/MemberFixture.java
@@ -0,0 +1,45 @@
+package com.snowflakes.rednose.fixture;
+
+import com.snowflakes.rednose.entity.Member;
+
+public class MemberFixture {
+
+    private Long id;
+    private Long socialId;
+    private String nickname;
+    private String image = "https://image.com/virtual.png";
+    private boolean usable = true;
+
+    public static MemberFixture builder() {
+        return new MemberFixture();
+    }
+
+    public MemberFixture id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public MemberFixture socialId(Long socialId) {
+        this.socialId = socialId;
+        return this;
+    }
+
+    public MemberFixture nickname(String nickname) {
+        this.nickname = nickname;
+        return this;
+    }
+
+    public MemberFixture image(String image) {
+        this.image = image;
+        return this;
+    }
+
+    public MemberFixture usable(boolean usable) {
+        this.usable = usable;
+        return this;
+    }
+
+    public Member build() {
+        return Member.builder().id(id).socialId(socialId).nickname(nickname).image(image).usable(usable).build();
+    }
+}
