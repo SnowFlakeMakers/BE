125aa2f805999fef823a2f3c90d6981a7c499217
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-04 12:08:50 +0900

    feat: 좋아요 기록 not found errorCode 추가
    Related to: #3

--- src/main/java/com/snowflakes/rednose/exception/ErrorCode.java
+++ src/main/java/com/snowflakes/rednose/exception/ErrorCode.java
@@ -4,6 +4,7 @@ public enum ErrorCode {
 
     MEMBER_NOT_FOUND("회원을 찾을 수 없습니다."),
     STAMP_NOT_FOUND("우표를 찾을 수 없습니다."),
+    STAMP_LIKE_NOT_FOUND("해당 우표의 좋아요 기록을 찾을 수 없습니다."),
 
     STAMP_LIKE("이미 좋아요를 누른 우표입니다.");
 
