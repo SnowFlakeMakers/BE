d783e1003667a3732a782643180ebdd096554de8
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-05 04:09:38 +0900

    test: 우표 리스트 최신순 정렬 테스트 추가
    Related to: #4

---
+++ src/test/java/com/snowflakes/rednose/repository/stamp/StampRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.snowflakes.rednose.repository.stamp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.snowflakes.rednose.entity.Stamp;
+import com.snowflakes.rednose.support.RepositoryTest;
+import com.snowflakes.rednose.support.fixture.StampFixture;
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+
+
+@Slf4j
+@RepositoryTest
+class StampRepositoryTest {
+
+    @Autowired
+    private StampRepository stampRepository;
+
+
+    @DisplayName("우표 목록을 최신순으로 페이지에 맞게 조회할 수 있다")
+    @Test
+    void 우표_목록_최신순_조회() {
+        // given
+        Stamp stamp1 = StampFixture.builder().createdAt(LocalDateTime.now()).numberOfLikes(1).build().toStamp();
+        Stamp stamp2 = StampFixture.builder().createdAt(LocalDateTime.now().minusDays(1)).numberOfLikes(2)
+                .build().toStamp();
+        Stamp stamp3 = StampFixture.builder().createdAt(LocalDateTime.now().minusDays(2)).numberOfLikes(3)
+                .build().toStamp();
+
+        stampRepository.save(stamp1);
+        stampRepository.save(stamp2);
+        stampRepository.save(stamp3);
+
+        // when
+        Slice<Stamp> slice0 = stampRepository.findAll(PageRequest.of(0, 2, Sort.by("createdAt").descending()));
+        Slice<Stamp> slice1 = stampRepository.findAll(PageRequest.of(1, 2, Sort.by("createdAt").descending()));
+
+        // then
+        assertAll(
+                () -> assertThat(slice0.getContent()).containsExactly(stamp1, stamp2),
+                () -> assertThat(slice1.getContent()).containsExactly(stamp3)
+        );
+    }
+
+
+}
