b1a3ed3e9629a8ba3f86c203884a978da24886c2
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-05 04:15:56 +0900

    test: 픽스처 필드 final 추가
    Related to: #4

--- src/test/java/com/snowflakes/rednose/support/fixture/StampFixture.java
+++ src/test/java/com/snowflakes/rednose/support/fixture/StampFixture.java
@@ -8,10 +8,10 @@ import lombok.Getter;
 @Getter
 public class StampFixture {
 
-    private String name = "우표";
-    private String imageUrl = "https://image.com/virtual.png";
-    private LocalDateTime createdAt;
-    private int numberOfLikes;
+    private final String name = "우표";
+    private final String imageUrl = "https://image.com/virtual.png";
+    private final LocalDateTime createdAt;
+    private final int numberOfLikes;
 
     @Builder
     public StampFixture(LocalDateTime createdAt, int numberOfLikes) {
