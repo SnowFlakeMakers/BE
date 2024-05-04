d2b3a41cb60d47f27c00f390300f6f099b018b89
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-05 01:42:01 +0900

    feat: ShowStampsResponse 추가
    ShowStampsResponse 추가
    - 클라이언트에게 우표 게시판 데이터를 전달하기 위한 dto 추가
    
    Related to: #4

---
+++ src/main/java/com/snowflakes/rednose/dto/response/ShowStampsResponse.java
@@ -0,0 +1,20 @@
+package com.snowflakes.rednose.dto.response;
+
+import com.snowflakes.rednose.entity.Stamp;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.Builder;
+import org.springframework.data.domain.Slice;
+
+@Builder
+public class ShowStampsResponse {
+
+    private List<StampAtListResponse> stampList;
+
+    public static ShowStampsResponse from(Slice<Stamp> stamps) {
+        List<Stamp> content = stamps.getContent();
+        return ShowStampsResponse.builder()
+                .stampList(content.stream().map(StampAtListResponse::from).collect(Collectors.toList()))
+                .build();
+    }
+}
