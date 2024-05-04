bec2d63076c5fd1250ac0e29d4554c538c2dd026
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-05 04:12:09 +0900

    test: 우표 리스트 좋아요순 정렬 테스트 추가
    Related to: #4

--- src/test/java/com/snowflakes/rednose/repository/stamp/StampRepositoryTest.java
+++ src/test/java/com/snowflakes/rednose/repository/stamp/StampRepositoryTest.java
@@ -34,9 +34,10 @@ class StampRepositoryTest {
         Stamp stamp3 = StampFixture.builder().createdAt(LocalDateTime.now().minusDays(2)).numberOfLikes(3)
                 .build().toStamp();
 
+        stampRepository.save(stamp3);
         stampRepository.save(stamp1);
         stampRepository.save(stamp2);
-        stampRepository.save(stamp3);
+
 
         // when
         Slice<Stamp> slice0 = stampRepository.findAll(PageRequest.of(0, 2, Sort.by("createdAt").descending()));
@@ -46,6 +47,31 @@ class StampRepositoryTest {
         assertAll(
                 () -> assertThat(slice0.getContent()).containsExactly(stamp1, stamp2),
                 () -> assertThat(slice1.getContent()).containsExactly(stamp3)
+        );
+    }
+
+    @DisplayName("우표 목록을 좋아요 순으로 페이지에 맞게 조회할 수 있다")
+    @Test
+    void 우표_목록_좋아요순_조회() {
+        // given
+        Stamp stamp1 = StampFixture.builder().createdAt(LocalDateTime.now()).numberOfLikes(1).build().toStamp();
+        Stamp stamp2 = StampFixture.builder().createdAt(LocalDateTime.now().minusDays(1)).numberOfLikes(2)
+                .build().toStamp();
+        Stamp stamp3 = StampFixture.builder().createdAt(LocalDateTime.now().minusDays(2)).numberOfLikes(3)
+                .build().toStamp();
+
+        stampRepository.save(stamp2);
+        stampRepository.save(stamp3);
+        stampRepository.save(stamp1);
+
+        // when
+        Slice<Stamp> slice0 = stampRepository.findAll(PageRequest.of(0, 2, Sort.by("numberOfLikes").descending()));
+        Slice<Stamp> slice1 = stampRepository.findAll(PageRequest.of(1, 2, Sort.by("numberOfLikes").descending()));
+
+        // then
+        assertAll(
+                () -> assertThat(slice0.getContent()).containsExactly(stamp3, stamp2),
+                () -> assertThat(slice1.getContent()).containsExactly(stamp1)
         );
     }
 
