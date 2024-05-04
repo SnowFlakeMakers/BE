3cc7af0c0b99fc090282686edcd80cc90b5e6047
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-04 12:08:09 +0900

    feat: member가 존재하는지 조회하는 기능 추가
    Related to: #3

--- src/main/java/com/snowflakes/rednose/repository/MemberRepository.java
+++ src/main/java/com/snowflakes/rednose/repository/MemberRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findById(Long memberId);
+
+    boolean existsById(Long memberId);
 }
