93aef2b5526d4f291a338dd91da8235a8b801650
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-05 02:08:20 +0900

    fix: 불필요한 오버라이딩 삭제
    findAll(Pageable) 형태의 메서드는 JpaRepository에 이미 정의되어 있음
    
    Related to: #4

--- src/main/java/com/snowflakes/rednose/repository/stamp/StampRepository.java
+++ src/main/java/com/snowflakes/rednose/repository/stamp/StampRepository.java
@@ -1,13 +1,9 @@
 package com.snowflakes.rednose.repository.stamp;
 
 import com.snowflakes.rednose.entity.Stamp;
-import java.awt.print.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StampRepository extends JpaRepository<Stamp, Long> {
-
-    Slice<Stamp> findAll(Pageable pageable);
 }
