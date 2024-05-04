0b4946d253cdfcb3c06999352a41b80febb92b2c
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-05 03:24:57 +0900

    test: StampFixture 추가
    Related to: #4

---
+++ src/test/java/com/snowflakes/rednose/support/fixture/StampFixture.java
@@ -0,0 +1,27 @@
+package com.snowflakes.rednose.support.fixture;
+
+import com.snowflakes.rednose.entity.Stamp;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class StampFixture {
+    private static Long fixtureId = 1L;
+    private Long id = fixtureId++;
+    private String name = "우표";
+    private String imageUrl = "https://image.com/virtual.png";
+    private LocalDateTime createdAt;
+    private int numberOfLikes;
+
+    @Builder
+    public StampFixture(LocalDateTime createdAt, int numberOfLikes) {
+        this.createdAt = createdAt;
+        this.numberOfLikes = numberOfLikes;
+    }
+
+    public Stamp toStamp() {
+        return Stamp.builder().id(id).name(name).imageUrl(imageUrl).createdAt(createdAt).numberOfLikes(numberOfLikes)
+                .build();
+    }
+}
