734bde3e5374452e01a145055e48a2d282b101a6
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-06 13:03:25 +0900

    feat: ShowStampsResponse에 필드 추가
    페이지네이션에 필요한 필드 추가
    - 전체 페이지
    - 현재 페이지
    
    Related to: #4

--- src/main/java/com/snowflakes/rednose/dto/response/ShowStampsResponse.java
+++ src/main/java/com/snowflakes/rednose/dto/response/ShowStampsResponse.java
@@ -5,18 +5,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Page;
 
 @Builder
 @Getter
 public class ShowStampsResponse {
 
     private List<StampAtListResponse> stampList;
+    private int pageNumber;
+    private int totalPages;
 
-    public static ShowStampsResponse from(Slice<Stamp> stamps) {
-        List<Stamp> content = stamps.getContent();
+    public static ShowStampsResponse from(Page<Stamp> stamps) {
         return ShowStampsResponse.builder()
-                .stampList(content.stream().map(StampAtListResponse::from).collect(Collectors.toList()))
+                .pageNumber(stamps.getPageable().getPageNumber())
+                .totalPages(stamps.getTotalPages())
+                .stampList(stamps.getContent().stream().map(StampAtListResponse::from).collect(Collectors.toList()))
                 .build();
     }
 }
