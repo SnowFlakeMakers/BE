0c595727b6999167afaf22f26fd5c8a20cc593fd
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-06 13:04:08 +0900

    feat: StampFixture 수정
    픽스처 필드 및 빌더 수정
    - createAt에 기본값
    - @Builder 애노테이션 삭제 및 빌더 메서드 선언
    
    Related to: #4

--- src/test/java/com/snowflakes/rednose/support/fixture/StampFixture.java
+++ src/test/java/com/snowflakes/rednose/support/fixture/StampFixture.java
@@ -2,24 +2,33 @@ package com.snowflakes.rednose.support.fixture;
 
 import com.snowflakes.rednose.entity.Stamp;
 import java.time.LocalDateTime;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class StampFixture {
 
     private final String name = "우표";
     private final String imageUrl = "https://image.com/virtual.png";
-    private final LocalDateTime createdAt;
-    private final int numberOfLikes;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private int numberOfLikes;
 
-    @Builder
-    public StampFixture(LocalDateTime createdAt, int numberOfLikes) {
-        this.createdAt = createdAt;
-        this.numberOfLikes = numberOfLikes;
+    public static StampFixture builder() {
+        return new StampFixture();
     }
 
-    public Stamp toStamp() {
+    public StampFixture createdAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
+
+    public StampFixture numberOfLikes(int numberOfLikes) {
+        this.numberOfLikes = numberOfLikes;
+        return this;
+    }
+
+    public Stamp build() {
         return Stamp.builder().name(name).imageUrl(imageUrl).createdAt(createdAt).numberOfLikes(numberOfLikes)
                 .build();
     }
