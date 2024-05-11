d885f10346879795436c11d0d184dd703994c1e4
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-11 18:06:18 +0900

    feat: AuthService 추가 및 getToken 추가
    인가 코드를 카카오 auth 서버에 post 요청을 보내 토큰을 반환하는 메서드 추가
    
    related to: #8

---
+++ src/main/java/com/snowflakes/rednose/service/AuthService.java
@@ -0,0 +1,44 @@
+package com.snowflakes.rednose.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@Transactional
+public class AuthService {
+
+    @Value("${client_id}")
+    private static String clientId;
+
+    /**
+     * 인가 코드를 받아 카카오 인증 서버에 post 요청을 보내고 토큰을 반환한다
+     *
+     * @param authCode 인가 코드
+     * @return KaKoToken
+     */
+    public KaKaoToken getToken(String authCode) {
+        WebClient webClient = WebClient.builder().build();
+
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+        requestBody.add("grant_type", "authorization_code");
+        requestBody.add("client_id", clientId);
+        requestBody.add("redirect_url", "http://localhost:8080/api/v1/login/kakao");
+        requestBody.add("code", "authCode");
+
+        KaKaoToken kaKaoToken = webClient.post()
+                .uri("https://kauth.kakao.com/oauth/token")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData(requestBody))
+                .retrieve()
+                .bodyToMono(KaKaoToken.class);
+
+        return kaKaoToken;
+    }
+}
+
