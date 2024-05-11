6c87f14be7a0a59238e8c4669ea37073cca6ea4c
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-11 21:34:37 +0900

    feat: getUserInfo 추가
    카카오 access token으로 카카오 인증 서버에 get요청을 보내서 사용자 정보(소셜id 등)을 얻어오는 메서드 추가
    
    related to: #8

--- src/main/java/com/snowflakes/rednose/service/AuthService.java
+++ src/main/java/com/snowflakes/rednose/service/AuthService.java
@@ -1,8 +1,14 @@
 package com.snowflakes.rednose.service;
 
 import com.snowflakes.rednose.dto.auth.KakaoToken;
+import com.snowflakes.rednose.dto.auth.LoginResultResponse;
+import com.snowflakes.rednose.dto.auth.UserInfo;
+import com.snowflakes.rednose.repository.MemberRepository;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -12,20 +18,22 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class AuthService {
 
     @Value("${client_id}")
-    private static String clientId;
+    private String clientId;
+    private WebClient webClient = WebClient.builder().build();
+
+    private final MemberRepository memberRepository;
 
     /**
      * 인가 코드를 받아 카카오 인증 서버에 post 요청을 보내고 토큰을 반환한다
      *
      * @param authCode 인가 코드
-     * @return KaKoToken
+     * @return KakaoToken
      */
     public KakaoToken getToken(String authCode) {
-        WebClient webClient = WebClient.builder().build();
-
         MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
         requestBody.add("grant_type", "authorization_code");
         requestBody.add("client_id", clientId);
@@ -33,7 +41,7 @@ public class AuthService {
         requestBody.add("code", "authCode");
 
         KakaoToken kaKaoToken = webClient.post()
-                .uri("https://kauth.kakao.com/oauth/token")
+                .uri("https://kapi.kakao.com/v2/user/me")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(BodyInserters.fromFormData(requestBody))
                 .retrieve()
@@ -41,6 +49,21 @@ public class AuthService {
                 .block();
 
         return kaKaoToken;
+    }
+
+    public UserInfo getUserInfo(KakaoToken kakaoToken) {
+        UserInfo userInfo = webClient.get()
+                .uri("https://kapi.kakao.com/v2/user/me")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + kakaoToken.getAccess_token())
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .retrieve()
+                .bodyToMono(UserInfo.class)
+                .block();
+        return userInfo;
+    }
+
+    public LoginResultResponse kakaoLogin(UserInfo userinfo) {
+        memberRepository.findBySocialId(userinfo.getSocialId());
     }
 }
 
