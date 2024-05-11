f7d72596d422086a4e215f3c9003cec0c9d2752c
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-11 21:35:41 +0900

    feat: UserInfo 추가
    카카오 인증 서버에서 받아온 사용자 정보를 위한 dto UserInfo 추가
    
    related to: #8

---
+++ src/main/java/com/snowflakes/rednose/dto/auth/UserInfo.java
@@ -0,0 +1,12 @@
+package com.snowflakes.rednose.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@AllArgsConstructor
+@Getter
+public class UserInfo {
+    private Long socialId;
+}
