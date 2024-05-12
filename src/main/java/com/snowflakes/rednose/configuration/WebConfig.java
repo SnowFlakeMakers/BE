2c4fa5dcb6a6fff20178e846a87ef4ea14f8075e
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-12 18:25:45 +0900

    feat: WebConfig 추가
    인터셉터 등록을 위해 추가함
    
    related to: #8

---
+++ src/main/java/com/snowflakes/rednose/configuration/WebConfig.java
@@ -0,0 +1,20 @@
+package com.snowflakes.rednose.configuration;
+
+import com.snowflakes.rednose.interceptor.AuthInterceptor;
+import com.snowflakes.rednose.service.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor);
+    }
+}
