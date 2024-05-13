971bb48b8b8aa5876bf3f5d55dcb49bf5481de64
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-13 23:36:03 +0900

    feat: 웹 소켓 연결관련 리스너 추가
    연결/끊김 시 로그
    
    Related to: #10

---
+++ src/main/java/com/snowflakes/rednose/controller/StampCraftController.java
@@ -0,0 +1,30 @@
+package com.snowflakes.rednose.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class StampCraftController {
+
+    @EventListener
+    public void connectWebSocket(SessionConnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+        log.info("session connected => {}", sessionId);
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+        log.info("session disconnected => {}", sessionId);
+    }
+
+}
