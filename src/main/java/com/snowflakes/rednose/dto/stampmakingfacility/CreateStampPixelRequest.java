51a2c6dd3f0fbd10cdebb2e431f118dcd6a2184c
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-13 23:31:14 +0900

    feat: 우표 색칠 요청 dto 추가
    - x,y 좌표
    - 색
    
    Related to: #10

---
+++ src/main/java/com/snowflakes/rednose/dto/stampmakingfacility/CreateStampPixelRequest.java
@@ -0,0 +1,24 @@
+package com.snowflakes.rednose.dto.stampmakingfacility;
+
+public class CreateStampPixelRequest {
+
+    private int x;
+    private int y;
+    private String color;
+
+    protected CreateStampPixelRequest() {
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+}
