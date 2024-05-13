528da231363fee08a69b08b0260df24cbc10bbde
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-13 19:02:04 +0900

    feat: 토큰 재발급 메서드 추가(미완성)
    related to: #8

--- src/main/java/com/snowflakes/rednose/controller/AuthController.java
+++ src/main/java/com/snowflakes/rednose/controller/AuthController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,4 +36,20 @@ public class AuthController {
 
         return ResponseEntity.status(HttpStatus.OK).body(loginResultResponse);
     }
+
+    @AccessibleWithoutLogin
+    @RequestMapping("/reissue/kakao")
+    public ResponseEntity<LoginResultResponse> kakaoReissue(@RequestHeader("Authentication") String authHeader) {
+        // 헤더에서 리프레시 토큰 얻기
+        String refreshToken = authHeader.split(" ")[1];
+
+        // 리프레시 토큰이 db에 있는 것과 일치하는지 검증
+        authService.validateRefreshToken(refreshToken);
+
+        // 검증하는 과정에서 에러를 던지지 않는다면 재발급
+
+        // 토큰 반환
+    }
+
+
 }
