d351936b40712a38df34f0a1c60661117e2018cf
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-14 23:54:29 +0900

    feat: 우표 제작소 생성 기능 추가
    - auth 개발 미완성으로 인한 임시 memberId 주입
    
    Related to: #10

--- src/main/java/com/snowflakes/rednose/controller/StampCraftController.java
+++ src/main/java/com/snowflakes/rednose/controller/StampCraftController.java
@@ -1,9 +1,14 @@
 package com.snowflakes.rednose.controller;
 
+import com.snowflakes.rednose.dto.stampcraft.CreateStampCraftRequest;
+import com.snowflakes.rednose.dto.stampcraft.CreateStampCraftResponse;
+import com.snowflakes.rednose.service.StampCraftService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
@@ -12,6 +17,13 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @RequiredArgsConstructor
 @Slf4j
 public class StampCraftController {
+
+    private final StampCraftService stampCraftService;
+
+    @PostMapping("/api/v1/stamp-craft")
+    public CreateStampCraftResponse create(@RequestBody CreateStampCraftRequest request, Long memberId) {
+        return stampCraftService.create(request, memberId);
+    }
 
     @EventListener
     public void connectWebSocket(SessionConnectEvent event) {
