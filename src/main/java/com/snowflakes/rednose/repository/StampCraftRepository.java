4b65fd5b9142746b9135e0cd131beff6a1d75651
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-14 23:43:16 +0900

    feat: StampCraftRepository 추가
    Related to: #10

---
+++ src/main/java/com/snowflakes/rednose/repository/StampCraftRepository.java
@@ -0,0 +1,7 @@
+package com.snowflakes.rednose.repository;
+
+import com.snowflakes.rednose.entity.StampCraft;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StampCraftRepository extends JpaRepository<StampCraft, Long> {
+}
