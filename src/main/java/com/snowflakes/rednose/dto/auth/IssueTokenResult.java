b8a8a800dd37edd398995b29a1cb28899449d417
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-15 19:45:34 +0900

    test: AuthServiceTest 추가
    related to: #8

--- src/main/java/com/snowflakes/rednose/dto/auth/IssueTokenResult.java
+++ src/main/java/com/snowflakes/rednose/dto/auth/IssueTokenResult.java
@@ -1,12 +1,14 @@
 package com.snowflakes.rednose.dto.auth;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @Builder
 @ToString
+@EqualsAndHashCode
 public class IssueTokenResult {
     private String accessToken;
     private String refreshTokenCookie;
