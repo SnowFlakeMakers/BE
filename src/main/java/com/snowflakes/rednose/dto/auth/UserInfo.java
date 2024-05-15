c0c2f181ebfef5f5418edff5f0399838d7dfcc5b
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-15 20:08:12 +0900

    feat: 필요 애노테이션 추가
    related to: #8

--- src/main/java/com/snowflakes/rednose/dto/auth/UserInfo.java
+++ src/main/java/com/snowflakes/rednose/dto/auth/UserInfo.java
@@ -1,6 +1,7 @@
 package com.snowflakes.rednose.dto.auth;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Builder
 public class UserInfo {
     private Long id;
 }
--- src/main/java/com/snowflakes/rednose/dto/member/SignInRequest.java
+++ src/main/java/com/snowflakes/rednose/dto/member/SignInRequest.java
@@ -1,14 +1,17 @@
 package com.snowflakes.rednose.dto.member;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class SignInRequest {
 
     @NotBlank(message = "닉네임은 빈칸일 수 없습니다")
     private String nickname;
+    @NotBlank(message = "socialId를 입력해 주세요")
     private Long socialId;
 }
