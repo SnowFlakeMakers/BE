d062e0fdabeb0426b4d790dd1dc6a4b300e6efb7
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-15 20:32:57 +0900

    feat: 예외 관련 수정
    예외 관련 수정
    - SignInRequest 필드 검증 애노테이션과 메세지 수정
    - CommonErrorCode 추가
    - GlobalExceptionHandler가 ResponseEntityExceptionHandler 상속받도록 수정
    - GlobalExceptionHandler에 기타 예외 잡기 위한 메서드 추가
    
    related to: #8

--- src/main/java/com/snowflakes/rednose/dto/member/SignInRequest.java
+++ src/main/java/com/snowflakes/rednose/dto/member/SignInRequest.java
@@ -2,6 +2,7 @@ package com.snowflakes.rednose.dto.member;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +15,6 @@ public class SignInRequest {
     @NotBlank(message = "닉네임은 빈칸일 수 없습니다")
     private String nickname;
 
-    @NotNull(message = "socialId는 빈칸일 수 없습니다")
+    @Positive(message = "socialId는 음수일 수 없습니다")
     private Long socialId;
 }
