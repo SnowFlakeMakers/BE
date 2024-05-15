af53026e4119570474ece40a6a567d1f4a51a8b2
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-15 19:26:52 +0900

    feat: 우표 제작소를 찾을 수 없는 경우 예외 추가
    Related to: #10

--- src/main/java/com/snowflakes/rednose/exception/ErrorCode.java
+++ src/main/java/com/snowflakes/rednose/exception/ErrorCode.java
@@ -6,7 +6,8 @@ public enum ErrorCode {
     STAMP_NOT_FOUND("우표를 찾을 수 없습니다."),
     STAMP_LIKE_NOT_FOUND("해당 우표의 좋아요 기록을 찾을 수 없습니다."),
 
-    STAMP_LIKE("이미 좋아요를 누른 우표입니다.");
+    STAMP_LIKE("이미 좋아요를 누른 우표입니다."),
+    STAMP_CRAFT_NOT_FOUND("우표 제작소를 찾을 수 없습니다.");
 
     private final String message;
 
