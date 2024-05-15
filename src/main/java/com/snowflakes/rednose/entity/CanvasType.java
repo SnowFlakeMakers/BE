d5e5f05e63b99922ae72918992e11375a411f7fc
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-15 18:53:08 +0900

    feat: 캔버스 크기에 구체적인 픽셀 수 추가
    Related to: #10

--- src/main/java/com/snowflakes/rednose/entity/CanvasType.java
+++ src/main/java/com/snowflakes/rednose/entity/CanvasType.java
@@ -1,5 +1,17 @@
 package com.snowflakes.rednose.entity;
 
 public enum CanvasType {
-    SMALL, MEDIUM, LARGE
+    SMALL(16),
+    MEDIUM(32),
+    LARGE(64);
+
+    private final int length;
+
+    CanvasType(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
 }
