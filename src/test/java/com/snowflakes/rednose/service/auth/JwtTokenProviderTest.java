18038ec9174dd8164e6b44a20173be9a8896185c
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-15 16:14:43 +0900

    test: 토큰 만료 테스트 추가
    related to: #8

--- src/test/java/com/snowflakes/rednose/service/auth/JwtTokenProviderTest.java
+++ src/test/java/com/snowflakes/rednose/service/auth/JwtTokenProviderTest.java
@@ -3,7 +3,9 @@ package com.snowflakes.rednose.service.auth;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.snowflakes.rednose.entity.Member;
+import com.snowflakes.rednose.exception.UnAuthorizedException;
 import com.snowflakes.rednose.support.fixture.MemberFixture;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -11,11 +13,17 @@ import org.junit.jupiter.api.Test;
 
 @Slf4j
 class JwtTokenProviderTest {
+    private final String FAKE_SECRET_KEY = "tlqkfdhodksehlsmsrjdi";
+    private final Long ACCESS_TOKEN_VALID_TIME = 1800000L;
+    private final Long REFRESH_TOKEN_VALID_TIME = 1800000L;
 
+    private final Long SHORT_TIME = 1000L;
 
-    private final String secretKey = "tlqkfdhodksehlsmsrjdi";
+    private final JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(FAKE_SECRET_KEY, ACCESS_TOKEN_VALID_TIME,
+            REFRESH_TOKEN_VALID_TIME);
 
-    private final JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(secretKey);
+    private final JwtTokenProvider jwtShortTokenProvider = new JwtTokenProvider(FAKE_SECRET_KEY, SHORT_TIME,
+            REFRESH_TOKEN_VALID_TIME);
 
     @DisplayName("access token을 발행하고 검증할 수 있다")
     @Test
@@ -42,6 +50,23 @@ class JwtTokenProviderTest {
 
         // then (검증 과정에서 예외를 던지지 않는 것을 테스트)
         Assertions.assertDoesNotThrow(() -> jwtTokenProvider.verifySignature(refreshToken));
+    }
+
+    @DisplayName("만료된 토큰에 대해 알맞은 예외를 던진다")
+    @Test
+    void token_만료() throws InterruptedException {
+        // given
+        final Long MEMBER_ID = 1L;
+        Member member = MemberFixture.builder().id(MEMBER_ID).build();
+
+        // when
+        String accessToken = jwtShortTokenProvider.createAccessToken(member);
+        Assertions.assertDoesNotThrow(() -> jwtShortTokenProvider.verifySignature(accessToken));
+
+        TimeUnit.MILLISECONDS.sleep(10000);
+
+        // then
+        Assertions.assertThrows(UnAuthorizedException.class, () -> jwtTokenProvider.verifySignature(accessToken));
     }
 
 
