8b54c12f6d07d75ee25107dcb4680dc16e285524
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-17 16:38:12 +0900

    feat: 마이페이지의 내가 만든 우표 조회 기능 추가
    Related to: #10

--- src/main/java/com/snowflakes/rednose/service/StampService.java
+++ src/main/java/com/snowflakes/rednose/service/StampService.java
@@ -1,26 +1,16 @@
 package com.snowflakes.rednose.service;
 
-import com.amazonaws.HttpMethod;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.Headers;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.snowflakes.rednose.dto.response.ShowStampsResponse;
-import com.snowflakes.rednose.dto.stamp.CreatePreSignedUrlRequest;
-import com.snowflakes.rednose.dto.stamp.CreatePreSignedUrlResponse;
+import com.snowflakes.rednose.dto.stamp.ShowMyStampsResponse;
 import com.snowflakes.rednose.entity.Stamp;
 import com.snowflakes.rednose.repository.stamp.StampRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.net.URL;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
@@ -35,4 +25,8 @@ public class StampService {
         return ShowStampsResponse.from(stamps);
     }
 
+    public ShowMyStampsResponse showMyStamps(Pageable pageable, Long memberId) {
+        Slice<Stamp> stamps = stampRepository.findMyStampsByMemberId(memberId, pageable);
+        return ShowMyStampsResponse.from(stamps);
+    }
 }
