ded9db66115b1b311fd86e153053cd3ef3a5aa87
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-19 00:36:09 +0900

    test: 이미 씰 누른 경우 예외 테스트
    Related to: #17

--- src/test/java/com/snowflakes/rednose/service/SealLikeServiceTest.java
+++ src/test/java/com/snowflakes/rednose/service/SealLikeServiceTest.java
@@ -3,6 +3,7 @@ package com.snowflakes.rednose.service;
 import com.snowflakes.rednose.entity.Member;
 import com.snowflakes.rednose.entity.Seal;
 import com.snowflakes.rednose.entity.SealLike;
+import com.snowflakes.rednose.exception.BadRequestException;
 import com.snowflakes.rednose.repository.MemberRepository;
 import com.snowflakes.rednose.repository.SealLikeRepository;
 import com.snowflakes.rednose.repository.SealRepository;
@@ -14,6 +15,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -42,6 +45,23 @@ class SealLikeServiceTest {
                 () -> verify(memberRepository, times(1)).findById(member.getId()),
                 () -> verify(sealRepository, times(1)).findById(seal.getId()),
                 () -> verify(sealLikeRepository, times(1)).save(any(SealLike.class))
+        );
+    }
+
+    @Test
+    void 이미_좋아요를_누른_경우_예외() {
+        Member member = MemberFixture.builder().build();
+        Seal seal = SealFixture.builder().build();
+        given(memberRepository.findById(member.getId())).willReturn(Optional.of(member));
+        given(sealRepository.findById(seal.getId())).willReturn(Optional.of(seal));
+        given(sealLikeRepository.existsByMemberIdAndSealId(member.getId(), seal.getId())).willReturn(true);
+
+        assertAll(
+                () -> assertThatThrownBy(() -> sealLikeService.like(seal.getId(), member.getId()))
+                        .isExactlyInstanceOf(BadRequestException.class),
+                () -> verify(memberRepository, times(1)).findById(member.getId()),
+                () -> verify(sealRepository, times(1)).findById(seal.getId()),
+                () -> verify(sealLikeRepository, times(0)).save(any(SealLike.class))
         );
     }
 
