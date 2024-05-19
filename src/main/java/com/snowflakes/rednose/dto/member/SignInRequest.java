cecf95a47db8a924a68081c98d71a397d4b73d9a
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-19 15:52:39 +0900

    refactor: dto에서 불필요한 필드 삭제
    related to: #8

--- src/main/java/com/snowflakes/rednose/dto/member/SignInRequest.java
+++ src/main/java/com/snowflakes/rednose/dto/member/SignInRequest.java
@@ -1,8 +1,6 @@
 package com.snowflakes.rednose.dto.member;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +12,4 @@ public class SignInRequest {
 
     @NotBlank(message = "닉네임은 빈칸일 수 없습니다")
     private String nickname;
-
-    @Positive(message = "socialId는 음수일 수 없습니다")
-    private Long socialId;
 }
