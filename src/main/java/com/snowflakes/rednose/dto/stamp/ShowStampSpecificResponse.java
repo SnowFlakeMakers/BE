473c3eb99b9852e9b4385ef55b60dded13dd850c
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-19 13:45:25 +0900

    feat: 우표 자세히 보기 응답 dto 추가
    related to: #8

--- src/main/java/com/snowflakes/rednose/dto/stamp/ShowStampSpecificResponse.java
+++ src/main/java/com/snowflakes/rednose/dto/stamp/ShowStampSpecificResponse.java
@@ -2,6 +2,7 @@ package com.snowflakes.rednose.dto.stamp;
 
 
 import com.snowflakes.rednose.entity.Member;
+import com.snowflakes.rednose.entity.Stamp;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
@@ -14,5 +15,11 @@ public class ShowStampSpecificResponse {
     private String imageUrl;
     private LocalDateTime createdAt;
     private List<Member> collaborators;
+    private int numberOfLikes;
     private boolean liked;
+
+    public static ShowStampSpecificResponse of(Stamp stamp, boolean liked, List<Member> collaborators) {
+        return ShowStampSpecificResponse.builder().name(stamp.getName()).imageUrl(stamp.getImageUrl())
+                .createdAt(stamp.getCreatedAt()).collaborators(collaborators).numberOfLikes(stamp.getNumberOfLikes()).liked(liked).build();
+    }
 }
--- src/main/java/com/snowflakes/rednose/service/StampService.java
+++ src/main/java/com/snowflakes/rednose/service/StampService.java
@@ -1,8 +1,14 @@
 package com.snowflakes.rednose.service;
 
+import com.snowflakes.rednose.dto.stamp.ShowStampSpecificResponse;
 import com.snowflakes.rednose.dto.stamp.ShowStampsResponse;
 import com.snowflakes.rednose.entity.Stamp;
+import com.snowflakes.rednose.entity.StampLike;
+import com.snowflakes.rednose.exception.NotFoundException;
+import com.snowflakes.rednose.exception.errorcode.StampErrorCode;
+import com.snowflakes.rednose.repository.StampLikeRepository;
 import com.snowflakes.rednose.repository.stamp.StampRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -17,9 +23,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class StampService {
 
     private final StampRepository stampRepository;
+    private final StampLikeRepository stampLikeRepository;
 
     public ShowStampsResponse show(Pageable pageable) {
         Page<Stamp> stamps = stampRepository.findAll(pageable);
         return ShowStampsResponse.from(stamps);
+    }
+
+    public ShowStampSpecificResponse showSpecific(Long stampId, Long memberId) {
+        Stamp stamp = stampRepository.findById(stampId)
+                .orElseThrow(() -> new NotFoundException(StampErrorCode.NOT_FOUND));
+        boolean liked = stampLikeRepository.existsByMemberIdAndStampId(memberId, stampId);
+        return ShowStampSpecificResponse.of(stamp,liked);
     }
 }
