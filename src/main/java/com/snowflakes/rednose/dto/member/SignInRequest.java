ed4f9f6c519a4c37a4874edd80a06390a9de8837
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-19 16:11:21 +0900

    feat: 생성자 추가
    related to: #8

--- src/main/java/com/snowflakes/rednose/dto/member/SignInRequest.java
+++ src/main/java/com/snowflakes/rednose/dto/member/SignInRequest.java
@@ -1,13 +1,17 @@
 package com.snowflakes.rednose.dto.member;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignInRequest {
 
     @NotBlank(message = "닉네임은 빈칸일 수 없습니다")
