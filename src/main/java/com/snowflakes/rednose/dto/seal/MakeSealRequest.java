d3def8f3eb83dca2c0b2da086c83194af1ffa377
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-25 05:21:10 +0900

    feat: notblank 검증 추가
    related to: #21

--- src/main/java/com/snowflakes/rednose/dto/seal/MakeSealRequest.java
+++ src/main/java/com/snowflakes/rednose/dto/seal/MakeSealRequest.java
@@ -3,11 +3,13 @@ package com.snowflakes.rednose.dto.seal;
 
 import static lombok.AccessLevel.PROTECTED;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = PROTECTED)
 public class MakeSealRequest {
+    @NotBlank(message = "씰 이미지 경로가 비었습니다")
     private String image;
 }
