e6d9591e8a6db9d9d241f1dda5b76b9f9e26396b
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-24 17:00:46 +0900

    feat: 이미지 삽입용 pre-signed url 반환 기능 추가 및 pre-signed url 관련 로직 분리
    Related to: #23

--- src/main/java/com/snowflakes/rednose/service/StampCraftService.java
+++ src/main/java/com/snowflakes/rednose/service/StampCraftService.java
@@ -1,11 +1,5 @@
 package com.snowflakes.rednose.service;
 
-import com.amazonaws.HttpMethod;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.Headers;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
-import com.snowflakes.rednose.dto.stamp.CreatePreSignedUrlRequest;
 import com.snowflakes.rednose.dto.stamp.CreatePreSignedUrlResponse;
 import com.snowflakes.rednose.dto.stampcraft.CreateStampRequest;
 import com.snowflakes.rednose.dto.stampcraft.CreateStampCraftRequest;
@@ -25,20 +19,16 @@ import com.snowflakes.rednose.exception.errorcode.StampCraftErrorCode;
 import com.snowflakes.rednose.repository.MemberRepository;
 import com.snowflakes.rednose.repository.StampRecordRepository;
 import com.snowflakes.rednose.repository.stamp.StampRepository;
+import com.snowflakes.rednose.service.auth.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-import java.net.URL;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
@@ -50,10 +40,8 @@ public class StampCraftService {
     private final MemberRepository memberRepository;
     private final StampRecordRepository stampRecordRepository;
     private final StampRepository stampRepository;
-    private final AmazonS3 amazonS3;
-
-    @Value("${cloud.s3.bucket}")
-    private String bucket;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PreSignedUrlService preSignedUrlService;
 
     private Long ID = 0L;
     private Map<Long, StampCraft> stampCrafts = new ConcurrentHashMap<>();
@@ -130,50 +118,20 @@ public class StampCraftService {
         }
     }
 
-    public CreatePreSignedUrlResponse getPreSignedUrl(CreatePreSignedUrlRequest request) {
-        String path = createPath(request);
-        GeneratePresignedUrlRequest generatePresignedUrlRequest = getGeneratePreSignedUrlRequest(path);
-        URL url = amazonS3.generatePresignedUrl(generatePresignedUrlRequest);
-        return new CreatePreSignedUrlResponse(url.toString());
-    }
-
-    private GeneratePresignedUrlRequest getGeneratePreSignedUrlRequest(String path) {
-        GeneratePresignedUrlRequest generatePresignedUrlRequest =
-                new GeneratePresignedUrlRequest(bucket, path)
-                        .withMethod(HttpMethod.PUT)
-                        .withExpiration(getPreSignedUrlExpiration());
-        generatePresignedUrlRequest.addRequestParameter(
-                Headers.S3_CANNED_ACL,
-                CannedAccessControlList.PublicRead.toString());
-        return generatePresignedUrlRequest;
-    }
-
-    private Date getPreSignedUrlExpiration() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.MINUTE, 2);
-        return new Date(calendar.getTimeInMillis());
-    }
-
-
-    private String createPath(CreatePreSignedUrlRequest request) {
-        StringBuilder stringBuilder = new StringBuilder();
-        if (!request.getDirectoryName().isEmpty()) {
-            stringBuilder.append(request.getDirectoryName())
-                    .append("/");
-        }
-        return stringBuilder.append(UUID.randomUUID()).append(request.getFileName()).toString();
-    }
-
     public void connect(SessionConnectEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
         String sessionId = headerAccessor.getSessionId();
-        Long memberId = Long.parseLong(headerAccessor.getFirstNativeHeader("MemberId"));
-        connections.put(sessionId, memberId);
+        String accessToken = headerAccessor.getFirstNativeHeader("Authorization");
+        connections.put(sessionId, jwtTokenProvider.getMemberId(accessToken));
     }
 
     public void disconnect(SessionDisconnectEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
         String sessionId = headerAccessor.getSessionId();
         connections.remove(sessionId);
+    }
+
+    public CreatePreSignedUrlResponse getPreSignedUrl() {
+        return new CreatePreSignedUrlResponse(preSignedUrlService.getStampPreSignedUrlForPut());
     }
 }
