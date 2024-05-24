842f69b88cc20b7818fe992c3cc8b0b6c4d35ed7
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-25 05:21:24 +0900

    feat: makeSeal 응답 dto 추가
    related to: #21

---
+++ src/main/java/com/snowflakes/rednose/dto/MakeSealResponse.java
@@ -0,0 +1,12 @@
+package com.snowflakes.rednose.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class MakeSealResponse {
+    private Long sealId;
+}
