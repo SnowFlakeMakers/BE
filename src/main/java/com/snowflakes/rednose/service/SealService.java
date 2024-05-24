181f6e62e16c38ec6ec1e9001499ad0249a64b24
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-25 05:21:53 +0900

    feat: seal 만드는 서비스 로직 추가
    related to: #21

--- src/main/java/com/snowflakes/rednose/service/SealService.java
+++ src/main/java/com/snowflakes/rednose/service/SealService.java
@@ -1,12 +1,18 @@
 package com.snowflakes.rednose.service;
 
+import com.snowflakes.rednose.dto.MakeSealResponse;
+import com.snowflakes.rednose.dto.seal.MakeSealRequest;
 import com.snowflakes.rednose.dto.seal.ShowMySealsResponse;
 import com.snowflakes.rednose.dto.seal.ShowSealSpecificResponse;
+import com.snowflakes.rednose.entity.Member;
 import com.snowflakes.rednose.entity.Seal;
 import com.snowflakes.rednose.exception.NotFoundException;
+import com.snowflakes.rednose.exception.errorcode.MemberErrorCode;
 import com.snowflakes.rednose.exception.errorcode.SealErrorCode;
+import com.snowflakes.rednose.repository.MemberRepository;
 import com.snowflakes.rednose.repository.SealLikeRepository;
 import com.snowflakes.rednose.repository.SealRepository;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -19,6 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class SealService {
     private final SealRepository sealRepository;
     private final SealLikeRepository sealLikeRepository;
+    private final MemberRepository memberRepository;
 
     public ShowSealSpecificResponse showSpecific(Long sealId, Long memberId) {
         Seal seal = findSealById(sealId);
@@ -33,5 +40,18 @@ public class SealService {
     public ShowMySealsResponse showMySeals(Pageable pageable, Long memberId) {
         Slice<Seal> seals = sealRepository.findAllByMemberIdOrderByCreatedAtAsc(memberId, pageable);
         return ShowMySealsResponse.from(seals);
+    }
+
+    public MakeSealResponse make(Long memberId, MakeSealRequest makeSealRequest) {
+        Member member = findMemberById(memberId);
+        Seal seal = sealRepository.save(
+                Seal.builder().createdAt(LocalDateTime.now()).member(member).imageUrl(makeSealRequest.getImage())
+                        .numberOfLikes(0).build());
+        return MakeSealResponse.builder().sealId(seal.getId()).build();
+    }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new NotFoundException(MemberErrorCode.NOT_FOUND));
     }
 }
