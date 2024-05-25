394cdfd80e0576c5d0d1c75455d2089563e08544
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-05-25 23:22:52 +0900

    test: 검색 테스트 추가 및 페이징 테스트 수정
    related to: #22

--- src/test/java/com/snowflakes/rednose/repository/stamp/StampRepositoryTest.java
+++ src/test/java/com/snowflakes/rednose/repository/stamp/StampRepositoryTest.java
@@ -3,15 +3,16 @@ package com.snowflakes.rednose.repository.stamp;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import com.snowflakes.rednose.entity.Stamp;
 import com.snowflakes.rednose.entity.Member;
+import com.snowflakes.rednose.entity.Stamp;
 import com.snowflakes.rednose.entity.StampRecord;
 import com.snowflakes.rednose.repository.MemberRepository;
 import com.snowflakes.rednose.repository.StampRecordRepository;
 import com.snowflakes.rednose.support.RepositoryTest;
-import com.snowflakes.rednose.support.fixture.StampFixture;
 import com.snowflakes.rednose.support.fixture.MemberFixture;
+import com.snowflakes.rednose.support.fixture.StampFixture;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -62,9 +63,17 @@ class StampRepositoryTest {
         final Stamp ANNIVERSARY_STAMP = 저장(StampFixture.builder().createdAt(LocalDateTime.now().minusDays(1))
                 .build());
 
+        final Member JANG = 저장(MemberFixture.builder().nickname("장지담").build());
+
+        저장(StampRecord.builder().member(JANG).stamp(BIRTHDAY_STAMP).build());
+        저장(StampRecord.builder().member(JANG).stamp(CHRISTMAS_STAMP).build());
+        저장(StampRecord.builder().member(JANG).stamp(ANNIVERSARY_STAMP).build());
+
         // when
-        Page<Stamp> page0 = stampRepository.findAll(PageRequest.of(0, 2, Sort.by("createdAt").descending()));
-        Page<Stamp> page1 = stampRepository.findAll(PageRequest.of(1, 2, Sort.by("createdAt").descending()));
+        Page<Stamp> page0 = stampRepository.findAllAtBoard(null,
+                PageRequest.of(0, 2, Sort.by("createdAt").descending()));
+        Page<Stamp> page1 = stampRepository.findAllAtBoard(null,
+                PageRequest.of(1, 2, Sort.by("createdAt").descending()));
 
         // then
         assertAll(
@@ -83,9 +92,17 @@ class StampRepositoryTest {
         final Stamp ANNIVERSARY_STAMP = 저장(StampFixture.builder().numberOfLikes(2)
                 .build());
 
+        final Member JANG = 저장(MemberFixture.builder().nickname("장지담").build());
+
+        저장(StampRecord.builder().member(JANG).stamp(BIRTHDAY_STAMP).build());
+        저장(StampRecord.builder().member(JANG).stamp(CHRISTMAS_STAMP).build());
+        저장(StampRecord.builder().member(JANG).stamp(ANNIVERSARY_STAMP).build());
+
         // when
-        Slice<Stamp> slice0 = stampRepository.findAll(PageRequest.of(0, 2, Sort.by("numberOfLikes").descending()));
-        Slice<Stamp> slice1 = stampRepository.findAll(PageRequest.of(1, 2, Sort.by("numberOfLikes").descending()));
+        Slice<Stamp> slice0 = stampRepository.findAllAtBoard(null,
+                PageRequest.of(0, 2, Sort.by("numberOfLikes").descending()));
+        Slice<Stamp> slice1 = stampRepository.findAllAtBoard(null,
+                PageRequest.of(1, 2, Sort.by("numberOfLikes").descending()));
 
         // then
         assertAll(
@@ -119,6 +136,63 @@ class StampRepositoryTest {
                 () -> assertThat(stamps2.getContent()).containsExactly(stamp2)
         );
 
+    }
+
+
+    @DisplayName("우표게시판에서 검색할 수 있다")
+    @Test
+    void 우표게시판_검색가능() {
+        // given
+        final Member JANG = 저장(MemberFixture.builder().nickname("장지담").build());
+        final Member GIL = 저장(MemberFixture.builder().nickname("길가은").build());
+        final Member LEE = 저장(MemberFixture.builder().nickname("이지민").build());
+
+        final Stamp BIRTHDAY_STAMP = 저장(
+                StampFixture.builder().name("HBD 생일축하 우표").createdAt(LocalDateTime.now().minusDays(2))
+                        .build());
+        final Stamp CHRISTMAS_STAMP = 저장(
+                StampFixture.builder().name("외로운 크리스마스 우표").createdAt(LocalDateTime.now()).build());
+        final Stamp ANNIVERSARY_STAMP = 저장(
+                StampFixture.builder().name("우리 벌써 100일 우표").createdAt(LocalDateTime.now().minusDays(1))
+                        .build());
+        final Stamp JANG_STAMP = 저장(
+                StampFixture.builder().name("우표 이름이 장지담").createdAt(LocalDateTime.now().minusDays(1))
+                        .build());
+
+        저장(StampRecord.builder().member(GIL).stamp(JANG_STAMP).build());
+
+        저장(StampRecord.builder().member(JANG).stamp(BIRTHDAY_STAMP).build());
+        저장(StampRecord.builder().member(GIL).stamp(BIRTHDAY_STAMP).build());
+
+        저장(StampRecord.builder().member(JANG).stamp(CHRISTMAS_STAMP).build());
+
+        저장(StampRecord.builder().member(GIL).stamp(ANNIVERSARY_STAMP).build());
+        저장(StampRecord.builder().member(LEE).stamp(ANNIVERSARY_STAMP).build());
+
+        final PageRequest PAGE_REQUEST = PageRequest.of(0, 10);
+
+        // when
+        final List<Stamp> JANG_KEYWORD = stampRepository.findAllAtBoard(JANG.getNickname(), PAGE_REQUEST).getContent();
+        final List<Stamp> GIL_KEYWORD = stampRepository.findAllAtBoard(GIL.getNickname(), PAGE_REQUEST).getContent();
+        final List<Stamp> LEE_KEYWORD = stampRepository.findAllAtBoard(LEE.getNickname(), PAGE_REQUEST).getContent();
+
+        // then
+        assertAll(
+                () -> assertThat(JANG_KEYWORD).contains(JANG_STAMP, BIRTHDAY_STAMP, CHRISTMAS_STAMP),
+                () -> assertThat(JANG_KEYWORD.size()).isEqualTo(3),
+                () -> assertThat(GIL_KEYWORD).contains(JANG_STAMP, BIRTHDAY_STAMP, ANNIVERSARY_STAMP),
+                () -> assertThat(GIL_KEYWORD.size()).isEqualTo(3),
+                () -> assertThat(LEE_KEYWORD).contains(ANNIVERSARY_STAMP),
+                () -> assertThat(LEE_KEYWORD.size()).isEqualTo(1)
+        );
+    }
+
+    private Member 저장(Member member) {
+        return memberRepository.save(member);
+    }
+
+    private StampRecord 저장(StampRecord stampRecord) {
+        return stampRecordRepository.save(stampRecord);
     }
 
 
