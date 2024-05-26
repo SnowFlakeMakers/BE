7cbde68dbd82053bc97bc787d1e98ed3b410155d
rlfrkdms1 <rlfrkdms1@naver.com>
2024-05-26 15:08:11 +0900

    refactor: 에러 메세지 수정
    Related to:

--- src/main/java/com/snowflakes/rednose/exception/errorcode/StampLikeErrorCode.java
+++ src/main/java/com/snowflakes/rednose/exception/errorcode/StampLikeErrorCode.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum StampLikeErrorCode implements ErrorCode {
 
-    ALREADY_EXIST("존재하는 우표입니다"),
+    ALREADY_EXIST("이미 좋아요한 우표입니다."),
     NOT_FOUND("존재하지 않는 좋아요입니다");
 
     private final String message;
