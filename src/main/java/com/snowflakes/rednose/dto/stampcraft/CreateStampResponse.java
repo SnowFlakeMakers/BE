9d028ebfc1e28da405f467331801748b2637e6d0
rlfrkdms1 <rlfrkdms1@naver.com>
2024-06-01 23:52:11 +0900

    feat: 완성시 type 추가
    Related to: #45

--- src/main/java/com/snowflakes/rednose/dto/stampcraft/CreateStampResponse.java
+++ src/main/java/com/snowflakes/rednose/dto/stampcraft/CreateStampResponse.java
@@ -2,37 +2,33 @@ package com.snowflakes.rednose.dto.stampcraft;
 
 import com.snowflakes.rednose.entity.Stamp;
 import lombok.Builder;
+import lombok.Getter;
 
+import static com.snowflakes.rednose.dto.stampcraft.MessageType.DONE;
+
+@Getter
 public class CreateStampResponse {
 
     private String image;
     private String name;
     private Long id;
+    private MessageType messageType;
 
     @Builder
-    public CreateStampResponse(String image, String name, Long id) {
+    public CreateStampResponse(String image, String name, Long id, MessageType messageType) {
         this.image = image;
         this.name = name;
         this.id = id;
+        this.messageType = messageType;
     }
 
-    public static CreateStampResponse from(Stamp stamp) {
+    public static CreateStampResponse of(Stamp stamp, String image) {
         return CreateStampResponse.builder()
-                .image(stamp.getImageUrl())
+                .image(image)
                 .name(stamp.getName())
                 .id(stamp.getId())
+                .messageType(DONE)
                 .build();
     }
 
-    public String getImage() {
-        return image;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
