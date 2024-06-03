6cf0492940fdeb94dfdc8779722becc4bea179c6
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-06-03 20:40:26 +0900

    refactor: 로그인 후 리다이렉트 되도록 수정
    related to: #48

--- src/main/java/com/snowflakes/rednose/controller/AuthController.java
+++ src/main/java/com/snowflakes/rednose/controller/AuthController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,14 +29,23 @@ public class AuthController {
 
     private final AuthService authService;
 
+    // 프론트가 아직 배포되지 않아 백으로 리다이렉트하고 정상적으로 리다이렉트되는지 확인하기 위함. 프론트 배포 후 url 수정 예정
+    private final String FRONT_HOMEPAGE = "http://localhost:8080/api/v1/redirectTest";
+
+
     @AccessibleWithoutLogin
     @GetMapping("/login/kakao")
     public ResponseEntity<LoginResultResponse> kakaoLogin(@RequestParam String code) {
         UserInfo userInfo = authService.getUserInfoFromAuthCode(code);
         IssueTokenResult issueTokenResult = authService.issueTokenWithUserInfo(userInfo);
-        return ResponseEntity.status(HttpStatus.OK)
+        return buildLoginResultResponse(issueTokenResult);
+    }
+
+    private ResponseEntity<LoginResultResponse> buildLoginResultResponse(IssueTokenResult issueTokenResult) {
+        return ResponseEntity.status(HttpStatus.FOUND)
                 .header(HttpHeaders.SET_COOKIE, issueTokenResult.getRefreshTokenCookie(),
                         issueTokenResult.getAccessTokenCookie())
+                .header(HttpHeaders.LOCATION, FRONT_HOMEPAGE)
                 .body(LoginResultResponse.from(issueTokenResult));
     }
 
@@ -43,10 +54,7 @@ public class AuthController {
     public ResponseEntity<LoginResultResponse> kakaoReissue(
             @CookieValue("refreshToken") String refreshToken) {
         IssueTokenResult issueTokenResult = authService.reIssueToken(refreshToken);
-        return ResponseEntity.status(HttpStatus.OK)
-                .header(HttpHeaders.SET_COOKIE, issueTokenResult.getRefreshTokenCookie(),
-                        issueTokenResult.getAccessTokenCookie())
-                .body(LoginResultResponse.from(issueTokenResult));
+        return buildLoginResultResponse(issueTokenResult);
     }
 
     @DeleteMapping("/logout")
@@ -59,5 +67,18 @@ public class AuthController {
     @GetMapping("/test")
     public String test(@RequestParam(defaultValue = "1") Long memberId) {
         return authService.issueAccessToken(memberId);
+    }
+
+
+    // 리다이렉트 테스트용 임시 메서드입니다
+    @AccessibleWithoutLogin
+    @GetMapping("/redirectTest")
+    public String redirectTest(@RequestHeader("accessToken") String accessToken,
+                               @RequestHeader("refreshToken") String refreshToken,
+                               @RequestBody LoginResultResponse loginResultResponse) {
+        log.info("redirect successful");
+        String result = String.format("accessToken : %s \nrefreshToken : %s\nnickname : %s\nimageUrl : %s", accessToken,
+                refreshToken, loginResultResponse.getNickname(), loginResultResponse.getImageUrl());
+        return result;
     }
 }
--- src/main/java/com/snowflakes/rednose/dto/auth/LoginResultResponse.java
+++ src/main/java/com/snowflakes/rednose/dto/auth/LoginResultResponse.java
@@ -1,10 +1,16 @@
 package com.snowflakes.rednose.dto.auth;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+// 아래 2개 애노테이션은 테스트를 위해 임시로 붙임
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class LoginResultResponse {
     private String imageUrl;
     private String nickname;
