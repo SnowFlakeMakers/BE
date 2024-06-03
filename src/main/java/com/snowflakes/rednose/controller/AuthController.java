daaa8646495bb8e0d4aa82f0b34a8594fe55245d
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-06-03 21:56:29 +0900

    refactor: 로그인 후 리다이렉트 할 때 이미지 경로는 쿠키에, 닉네임은 쿼리파라미터에 주도록 수정
    related to: #48

--- src/main/java/com/snowflakes/rednose/controller/AuthController.java
+++ src/main/java/com/snowflakes/rednose/controller/AuthController.java
@@ -3,9 +3,9 @@ package com.snowflakes.rednose.controller;
 import com.snowflakes.rednose.annotation.AccessibleWithoutLogin;
 import com.snowflakes.rednose.annotation.MemberId;
 import com.snowflakes.rednose.dto.auth.IssueTokenResult;
-import com.snowflakes.rednose.dto.auth.LoginResultResponse;
 import com.snowflakes.rednose.dto.auth.UserInfo;
 import com.snowflakes.rednose.service.auth.AuthService;
+import java.net.URLEncoder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,31 +25,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    public static final String REDIRECT_URL_FORMAT = "%s?nickname=%s";
     private final AuthService authService;
-
-    // 프론트가 아직 배포되지 않아 백으로 리다이렉트하고 정상적으로 리다이렉트되는지 확인하기 위함. 프론트 배포 후 url 수정 예정
-    private final String FRONT_HOMEPAGE = "http://localhost:8080/api/v1/redirectTest";
-
+    private final String FRONT_HOMEPAGE = "http://localhost:3000/home";
 
     @AccessibleWithoutLogin
     @GetMapping("/login/kakao")
-    public ResponseEntity<LoginResultResponse> kakaoLogin(@RequestParam String code) {
+    public ResponseEntity<Void> kakaoLogin(@RequestParam String code) {
         UserInfo userInfo = authService.getUserInfoFromAuthCode(code);
         IssueTokenResult issueTokenResult = authService.issueTokenWithUserInfo(userInfo);
         return buildLoginResultResponse(issueTokenResult);
     }
 
-    private ResponseEntity<LoginResultResponse> buildLoginResultResponse(IssueTokenResult issueTokenResult) {
+    private ResponseEntity<Void> buildLoginResultResponse(IssueTokenResult issueTokenResult) {
+        String REDIRECT_URL = String.format(REDIRECT_URL_FORMAT, FRONT_HOMEPAGE,
+                URLEncoder.encode(issueTokenResult.getNickname()));
         return ResponseEntity.status(HttpStatus.FOUND)
+                .header(HttpHeaders.LOCATION, REDIRECT_URL)
                 .header(HttpHeaders.SET_COOKIE, issueTokenResult.getRefreshTokenCookie(),
-                        issueTokenResult.getAccessTokenCookie())
-                .header(HttpHeaders.LOCATION, FRONT_HOMEPAGE)
-                .body(LoginResultResponse.from(issueTokenResult));
+                        issueTokenResult.getAccessTokenCookie(),
+                        issueTokenResult.getImageUrlCookie())
+                .build();
     }
 
     @AccessibleWithoutLogin
     @PostMapping("/reissue/kakao")
-    public ResponseEntity<LoginResultResponse> kakaoReissue(
+    public ResponseEntity<Void> kakaoReissue(
             @CookieValue("refreshToken") String refreshToken) {
         IssueTokenResult issueTokenResult = authService.reIssueToken(refreshToken);
         return buildLoginResultResponse(issueTokenResult);
@@ -67,18 +66,5 @@ public class AuthController {
     @GetMapping("/test")
     public String test(@RequestParam(defaultValue = "1") Long memberId) {
         return authService.issueAccessToken(memberId);
-    }
-
-
-    // 리다이렉트 테스트용 임시 메서드입니다
-    @AccessibleWithoutLogin
-    @GetMapping("/redirectTest")
-    public String redirectTest(@RequestHeader("accessToken") String accessToken,
-                               @RequestHeader("refreshToken") String refreshToken,
-                               @RequestBody LoginResultResponse loginResultResponse) {
-        log.info("redirect successful");
-        String result = String.format("accessToken : %s \nrefreshToken : %s\nnickname : %s\nimageUrl : %s", accessToken,
-                refreshToken, loginResultResponse.getNickname(), loginResultResponse.getImageUrl());
-        return result;
     }
 }
