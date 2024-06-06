85882c6339e1c290715367dd766db915d360495d
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-06-06 19:25:14 +0900

    refactor: 테스트에서 불필요해진 라인 삭제 및 수정
    related to: #55

--- src/test/java/com/snowflakes/rednose/service/auth/AuthServiceTest.java
+++ src/test/java/com/snowflakes/rednose/service/auth/AuthServiceTest.java
@@ -53,9 +53,11 @@ class AuthServiceTest {
         final String IMAGE_URL = "image.com";
         final String REFRESH_TOKEN_COOKIE =
                 "refreshToken=" + REFRESH_TOKEN + "; Path=/; Secure; HttpOnly; SameSite=None";
-        IssueTokenResult expected = IssueTokenResult.builder().refreshTokenCookie(REFRESH_TOKEN_COOKIE)
-                .accessToken(ACCESS_TOKEN)
-                .image(IMAGE_URL)
+        final String ACCESS_TOKEN_COOKIE =
+                "accessToken=" + REFRESH_TOKEN + "; Path=/; Secure; HttpOnly; SameSite=None";
+        IssueTokenResult expected = IssueTokenResult.builder()
+                .refreshTokenCookie(REFRESH_TOKEN_COOKIE)
+                .accessTokenCookie(ACCESS_TOKEN_COOKIE)
                 .build();
 
         Member member = MemberFixture.builder().id(MEMBER_ID).build();
