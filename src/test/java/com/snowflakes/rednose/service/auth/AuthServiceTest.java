6fb528ebc4a4c150fdd7c8f4689768f419be88d7
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-06-06 13:35:21 +0900

    test: 테스트 수정
    related to: #52

--- src/test/java/com/snowflakes/rednose/service/auth/AuthServiceTest.java
+++ src/test/java/com/snowflakes/rednose/service/auth/AuthServiceTest.java
@@ -53,11 +53,11 @@ class AuthServiceTest {
         final String IMAGE_URL = "image.com";
         final String REFRESH_TOKEN_COOKIE =
                 "refreshToken=" + REFRESH_TOKEN + "; Path=/; Secure; HttpOnly; SameSite=None";
-        final String ACCESS_TOKEN_COOKIE = "accessToken=" + REFRESH_TOKEN + "; Path=/; Secure; HttpOnly; SameSite=None";
-        final String IMAGE_URL_COOKIE = "imageUrl=" + IMAGE_URL + "; Path=/; Secure; HttpOnly; SameSite=None";
+     //   final String ACCESS_TOKEN_COOKIE = "accessToken=" + REFRESH_TOKEN + "; Path=/; Secure; HttpOnly; SameSite=None";
+     //   final String IMAGE_URL_COOKIE = "imageUrl=" + IMAGE_URL + "; Path=/; Secure; HttpOnly; SameSite=None";
         IssueTokenResult expected = IssueTokenResult.builder().refreshTokenCookie(REFRESH_TOKEN_COOKIE)
-                .accessTokenCookie(ACCESS_TOKEN_COOKIE)
-                .imageUrlCookie(IMAGE_URL_COOKIE)
+                .accessToken(ACCESS_TOKEN)
+                .image(IMAGE_URL)
                 .build();
 
         Member member = MemberFixture.builder().id(MEMBER_ID).build();
