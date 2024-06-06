320b1f41d2e40d76bbdd092eba9038cead14e09e
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-06-06 23:34:42 +0900

    test: 오탈자 수정
    related to: #55

--- src/test/java/com/snowflakes/rednose/service/auth/AuthServiceTest.java
+++ src/test/java/com/snowflakes/rednose/service/auth/AuthServiceTest.java
@@ -50,11 +50,10 @@ class AuthServiceTest {
         final Long MEMBER_ID = 1L;
         final String REFRESH_TOKEN = "flvmfptlxhzms123";
         final String ACCESS_TOKEN = "ajcptmxhzms123";
-        final String IMAGE_URL = "image.com";
         final String REFRESH_TOKEN_COOKIE =
                 "refreshToken=" + REFRESH_TOKEN + "; Path=/; Secure; HttpOnly; SameSite=None";
         final String ACCESS_TOKEN_COOKIE =
-                "accessToken=" + REFRESH_TOKEN + "; Path=/; Secure; HttpOnly; SameSite=None";
+                "accessToken=" + ACCESS_TOKEN + "; Path=/; Secure; HttpOnly; SameSite=None";
         IssueTokenResult expected = IssueTokenResult.builder()
                 .refreshTokenCookie(REFRESH_TOKEN_COOKIE)
                 .accessTokenCookie(ACCESS_TOKEN_COOKIE)
