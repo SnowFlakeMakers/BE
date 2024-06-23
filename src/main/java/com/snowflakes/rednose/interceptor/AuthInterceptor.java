71c4fcd7bc34c12ce3dcdac98eb85f0b737a2030
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-06-23 19:15:35 +0900

    fix: cookie가 null인 경우 예외처리 추가
    related to: #59

--- src/main/java/com/snowflakes/rednose/interceptor/AuthInterceptor.java
+++ src/main/java/com/snowflakes/rednose/interceptor/AuthInterceptor.java
@@ -4,6 +4,7 @@ import com.snowflakes.rednose.annotation.AccessibleWithoutLogin;
 import com.snowflakes.rednose.exception.UnAuthorizedException;
 import com.snowflakes.rednose.exception.errorcode.AuthErrorCode;
 import com.snowflakes.rednose.service.auth.JwtTokenProvider;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Arrays;
@@ -47,6 +48,10 @@ public class AuthInterceptor implements HandlerInterceptor {
     }
 
     private String getAccessTokenFromRequest(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            throw new UnAuthorizedException(AuthErrorCode.NULL_OR_BLANK_TOKEN);
+        }
         return Arrays.stream(request.getCookies())
                 .filter(cookie -> ACCESS_TOKEN.equals(cookie.getName()))
                 .findFirst()
