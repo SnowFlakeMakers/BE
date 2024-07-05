346a3ef2e806889f44622057ae9e096c9b916df7
rlfrkdms1 <rlfrkdms1@naver.com>
2024-07-05 16:35:37 +0900

    feat: stomp interceptor 등록
    Related to: #63

--- src/main/java/com/snowflakes/rednose/config/WebSocketConfig.java
+++ src/main/java/com/snowflakes/rednose/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.snowflakes.rednose.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -17,9 +18,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/connections")
+        registry.setErrorHandler(stompErrorHandler)
+                .addEndpoint("/connections")
                 .setAllowedOriginPatterns("*");
-        registry.setErrorHandler(stompErrorHandler);
     }
 
     @Override
@@ -28,4 +29,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/pub");
     }
 
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
+    }
 }
